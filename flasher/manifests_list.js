a528b0af6fc5b7f18eb2881a90cdeb345ef061fa
Marcelin Delcour <84352095+marcelinDelcour@users.noreply.github.com>
2025-12-27 10:33:42 +0100

    version 1.1.0 (#8)
    * MDE: correction index.html & manifest
    
    * MDE: create version 1.0
    
    * add wifi images
    
    * add wifi images
    
    * add wifi images
    
    * add wifi images
    
    * add cg340 images
    
    * MDE: add vumetre v1.2.0
    
    * MDE: add vumetre v1.2.0
    
    * Update index.html
    
    * alpinejs integration +
    Button disabled by default
    
    * alpinejs code simplification
    
    * Refactor manifest selection logic and add manifests_list.js for dynamic options
    
    * MDE: add vu-metre v1.3.0
    
    * MDE: remove vu-metre v1.0
    
    * MDE: remove manufest vu-metre v1.0
    
    ---------
    
    Co-authored-by: technoLARP <technolarp@gmail.com>
    Co-authored-by: JosefRDA <josef.rda@gmail.com>
    Co-authored-by: Romain Féret-Bosquelle <romain.feret@gmail.com>
    Co-authored-by: mdelcour <mdelcour@oxya.com>

---
+++ flasher/manifests_list.js
@@ -0,0 +1,84 @@
+var manfestList = [
+    {
+        "object" : "bombe_7segment_01",
+        "versions" : [
+            {
+                "id" : "latest",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/bombe_7segment_01.manifest.json"
+            }
+        ] 
+    },
+    {
+        "object" : "cloneur_rfid_01",
+        "versions" : [
+            {
+                "id" : "latest",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/cloneur_rfid_01.manifest.json"
+            }
+        ] 
+    },
+    {
+        "object" : "panneau_01",
+        "versions" : [
+            {
+                "id" : "latest",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/panneau_01.manifest.json"
+            }
+        ]
+    },
+    {
+        "object" : "serrure_clavier_01",
+        "versions" : [
+            {
+                "id" : "latest",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/serrure_clavier_01.manifest.json"
+            }
+        ]
+    },
+    {
+        "object" : "serrure_medfan_01",
+        "versions" : [
+            {
+                "id" : "latest",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/serrure_medfan_01.manifest.json"
+            }
+        ]
+    },
+    {
+        "object" : "serrure_medfan_02",
+        "versions" : [
+            {
+                "id" : "latest",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/serrure_medfan_02.manifest.json"
+            }
+        ]
+    },
+    {
+        "object" : "serrure_rfid_01",
+        "versions" : [
+            {
+                "id" : "latest",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/serrure_rfid_01.manifest.json"
+            }
+        ]
+        
+    },
+    {
+        "object" : "vumetre_01",
+        "versions" : [
+            {
+                "id" : "latest",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/vumetre_01_v1.3.0.manifest.json"
+            },
+            {
+                "id" : "v1.2.0",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/vumetre_01_v1.2.0.manifest.json"
+            },
+            {
+                "id" : "v1.3.0",
+                "manifest" : "https://technolarp.github.io/flasher/manifest/vumetre_01_v1.3.0.manifest.json"
+            }
+        ]
+        
+    }
+]
